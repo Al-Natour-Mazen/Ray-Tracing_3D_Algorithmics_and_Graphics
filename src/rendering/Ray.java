66ab7b3f82be478b65ce1c970c07a867e130e40e
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-05-04 00:31:34 +0200

    Create Ray.java
    create this class to represente the rays in the scenes ( lights basically)

---
+++ src/rendering/Ray.java
@@ -0,0 +1,50 @@
+package rendering;
+
+import utils.MyColor;
+import utils.MyVec3;
+
+public class Ray {
+
+    private final MyVec3 position;
+    private final MyColor diffuse;
+    private final MyColor specular;
+
+    public static MyColor AMBIENT_LIGHT = MyColor.black;
+
+    /**
+     * Constructor.
+     *
+     * @param position the position of the Ray
+     * @param diffuse  the diffuse color of the Ray
+     * @param specular the specular color of the Ray
+     */
+    public Ray(MyVec3 position, MyColor diffuse, MyColor specular) {
+        this.position = position;
+        this.diffuse = diffuse;
+        this.specular = specular;
+    }
+
+    /**
+     *
+     * @return the position of the light
+     */
+    public MyVec3 getPosition() {
+        return position;
+    }
+
+    /**
+     *
+     * @return the diffuse color of the light
+     */
+    public MyColor getDiffuse() {
+        return diffuse;
+    }
+
+    /**
+     *
+     * @return the specular color of the light
+     */
+    public MyColor getSpecular() {
+        return specular;
+    }
+}
