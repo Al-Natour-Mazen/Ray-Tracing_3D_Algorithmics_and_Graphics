2221d3602130650cd1f7e83ee0f12b94ff15254f
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-05-04 00:30:49 +0200

    Create ClassicScene.java

---
+++ src/scenes/ClassicScene.java
@@ -0,0 +1,14 @@
+package scenes;
+
+public class ClassicScene extends Scene{
+
+    @Override
+    protected void initObjects() {
+
+    }
+
+    @Override
+    protected void initLights() {
+
+    }
+}
