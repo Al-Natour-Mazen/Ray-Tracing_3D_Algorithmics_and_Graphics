8fa458503a7e149a717c12649ba84aec5b78f44f
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-05-04 00:33:01 +0200

    Update IntersectableObject.java
    change class due to the last commit with the options

--- src/objects/IntersectableObject.java
+++ src/objects/IntersectableObject.java
@@ -1,20 +1,55 @@
 package objects;
 
-import utils.Color;
+import utils.MyVec3;
 
 public abstract class IntersectableObject  {
-    private final Color color;
-    private final Color specularColor;
+
+    protected IntersectObjectDrawOptions drawOptions;
 
     /**
      * Constructor.
      *
-     * @param color             the color of the object
-     * @param specularColor     the specular color of the object
+     * @param options the draw options
      */
-    public IntersectableObject (Color color, Color specularColor) {
-        this.color = color;
-        this.specularColor = specularColor;
-
+    public IntersectableObject (IntersectObjectDrawOptions options) {
+        this.drawOptions = options;
     }
+
+    /**
+     * Compute the intersection between the object and the ray.
+     *
+     * @param origin the origin (P) of the ray
+     * @param direction the direction (V) of the ray
+     * @return the intersection
+     */
+    public abstract double getIntersection(MyVec3 origin, MyVec3 direction);
+
+    /**
+     *
+     * @param I the intersection
+     * @return the normal of the object
+     */
+    public abstract MyVec3 getNormal(MyVec3 I);
+
+    /**
+     *
+     * @param origin the origin (P) of the ray
+     * @param direction the direction (V) of the ray
+     * @param lambdaI the distance from the origin of the ray to the intersection
+     *                point
+     * @return the intersection point
+     */
+    public MyVec3 getIntersectionPoint(MyVec3 origin, MyVec3 direction, double lambdaI) {
+        return origin.add(direction.mul(lambdaI));
+    }
+
+    /**
+     *
+     * @return the draw options of the object
+     */
+    public IntersectObjectDrawOptions getDrawOptions() {
+        return drawOptions;
+    }
+
+
 }
