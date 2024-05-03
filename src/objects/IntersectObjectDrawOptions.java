3e106e69ad21b979359a35a28b7b52ce100e435d
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-05-04 00:32:21 +0200

    Create IntersectObjectDrawOptions.java
    add this class to support all the object options like color, shininess, etcc...

---
+++ src/objects/IntersectObjectDrawOptions.java
@@ -0,0 +1,48 @@
+package objects;
+
+import utils.MyColor;
+import utils.MyVec3;
+
+public class IntersectObjectDrawOptions {
+
+    private final MyColor color;
+    private final MyColor specularColor;
+    private final double shininess;
+
+    /**
+     * Constructor.
+     *
+     * @param objColor         the color of the object
+     * @param specularColor the specular color of the object
+     */
+    public IntersectObjectDrawOptions(MyColor objColor, MyColor specularColor, double shininess) {
+        this.color = objColor;
+        this.specularColor = specularColor;
+        this.shininess = shininess;
+    }
+
+    /**
+     *
+     * @param I the intersection point
+     * @return the color of the intersection point
+     */
+    public MyColor getColor(MyVec3 I) {
+        return color;
+    }
+
+    /**
+     *
+     * @return the specular color of the object
+     */
+    public MyColor getSpecularColor() {
+        return specularColor;
+    }
+
+    /**
+     *
+     * @return the shininess of the object
+     */
+    public double getShininess() {
+        return shininess;
+    }
+}
