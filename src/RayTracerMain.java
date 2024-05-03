b1fcec26459ff8af1b2f325374845dd2ab0f2ebe
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-05-03 18:06:12 +0200

    Update RayTracerMain.java

--- src/RayTracerMain.java
+++ src/RayTracerMain.java
@@ -1,5 +1,11 @@
-public class RayTracerMain {
+import utils.JavaTga;
+
+public class RayTracerMain extends JavaTga {
+
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+
 }
