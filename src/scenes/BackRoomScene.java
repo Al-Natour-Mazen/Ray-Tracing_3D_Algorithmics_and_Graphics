7eed170237b39685f575f22b628dc020e07df574
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-05-08 23:48:30 +0200

    Update BackRoomScene.java

--- src/scenes/BackRoomScene.java
+++ src/scenes/BackRoomScene.java
@@ -17,9 +17,9 @@ public class BackRoomScene extends Scene {
         IntersectableObjectDrawableOptions sphereOptions = new IntersectableObjectDrawableOptions(MyColor.black, MyColor.white, 10.0D);
 
         // Creation of the walls
-        IntersectableObject wall1 = new Plan(wallOptions, new MyVec3(0.0D, 10.0D, 0.0D), 10.0D); // Mur droit
-        IntersectableObject wall2 = new Plan(wallOptions, new MyVec3(0.0D, -10.0D, 0.0D), 10.0D); // Mur gauche
-        IntersectableObject wall3 = new Plan(wallOptions, new MyVec3(10.0D, 0.0D, 0.0D), 10.0D); // Mur de droite
+        IntersectableObject wall1 = new Plan(wallOptions, new MyVec3(0.0D, 10.0D, 0.0D), 10.0D);
+        IntersectableObject wall2 = new Plan(wallOptions, new MyVec3(0.0D, -10.0D, 0.0D), 10.0D);
+        IntersectableObject wall3 = new Plan(wallOptions, new MyVec3(10.0D, 0.0D, 0.0D), 10.0D);
 
         // Creation of the spheres
         IntersectableObject sphere1 = new Sphere(sphereOptions, new MyVec3(30.0D, 0.0D, -40.0D), 1.0D);
