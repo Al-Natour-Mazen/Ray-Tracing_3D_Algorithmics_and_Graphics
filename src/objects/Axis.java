020519419e8b0c776c8dac4f2de03c48169a6526
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-05-08 23:49:05 +0200

    Create Axis.java
    add this new enum to handle rotation angle with the triangles

---
+++ src/objects/Axis.java
@@ -0,0 +1,11 @@
+package objects;
+
+/**
+ * Enum representing the three axes in a three-dimensional coordinate system: X, Y, and Z.
+ * It is used to specify the axis.
+ */
+public enum Axis {
+    X,  // The X-axis
+    Y,  // The Y-axis
+    Z   // The Z-axis
+}
