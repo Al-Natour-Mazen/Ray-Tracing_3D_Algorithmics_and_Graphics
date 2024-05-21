6ee864a512b2b9bdf129ecebae1fb89fda8249a4
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-05-22 00:31:28 +0200

    Update IntersectableObjectDrawableOptions.java
    add more variable to calculate refraction and reflexion, add more constructor and add getters

--- src/objects/IntersectableObjectDrawableOptions.java
+++ src/objects/IntersectableObjectDrawableOptions.java
@@ -8,6 +8,9 @@ public class IntersectableObjectDrawableOptions {
     private final MyColor color;
     private final MyColor specularColor;
     private final double shininess;
+    private double kr;
+    private double kt;
+    private double eta;
 
     /**
      * Constructor.
@@ -22,11 +25,57 @@ public class IntersectableObjectDrawableOptions {
     }
 
     /**
+     * Constructor.
+     *
+     * @param objColor          the color of the object
+     * @param specularColor     the specular color of the object
+     * @param shininess         the shininess of the object
+     * @param reflectionCoeff   the reflection coefficient of the object
+     * @param transmissionCoeff the transmission coefficient of the object
+     * @param refractionIndex   the refraction index of the object
+     */
+    public IntersectableObjectDrawableOptions(MyColor objColor, MyColor specularColor, double shininess, double reflectionCoeff,
+                               double transmissionCoeff, double refractionIndex) {
+        this.color = objColor;
+        this.specularColor = specularColor;
+        this.shininess = shininess;
+        this.kr = reflectionCoeff;
+        this.kt = transmissionCoeff;
+        this.eta = refractionIndex;
+    }
+
+    /**
+     * Constructor.
+     *
+     * @param objColor          the color of the object
+     * @param specularColor     the specular color of the object
+     * @param shininess         the shininess of the object
+     * @param reflectionCoeff   the reflection coefficient of the object
+     */
+    public IntersectableObjectDrawableOptions(MyColor objColor, MyColor specularColor, double shininess, double reflectionCoeff) {
+        this.color = objColor;
+        this.specularColor = specularColor;
+        this.shininess = shininess;
+        this.kr = reflectionCoeff;
+        this.kt = 0.0D;
+        this.eta = 0.0D;
+    }
+
+    /**
+     * Calculate the color of the intersection point.
      *
      * @param I the intersection point
      * @return the color of the intersection point
      */
-    public MyColor getColor(MyVec3 I) {
+    public MyColor calculateIntersectionColor(MyVec3 I) {
+        return color;
+    }
+
+    /**
+     *
+     * @return the main color of the object
+     */
+    public MyColor getColor() {
         return color;
     }
 
@@ -44,5 +93,29 @@ public class IntersectableObjectDrawableOptions {
      */
     public double getShininess() {
         return shininess;
+    }
+
+    /**
+     *
+     * @return the reflection coefficient of the object
+     */
+    public double getReflectionCoefficient() {
+        return kr;
+    }
+
+    /**
+     *
+     * @return the transmission coefficient of the object
+     */
+    public double getTransmissionCoefficient() {
+        return kt;
+    }
+
+    /**
+     *
+     * @return the refraction index of the object
+     */
+    public double getRefractionIndex() {
+        return eta;
     }
 }
