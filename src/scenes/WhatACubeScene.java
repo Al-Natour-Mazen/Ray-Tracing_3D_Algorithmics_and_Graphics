9ac3b65dacd9dc83e0e4d94e40652ac30b9d2728
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-05-22 18:09:41 +0200

    Update WhatACubeScene.java

--- src/scenes/WhatACubeScene.java
+++ src/scenes/WhatACubeScene.java
@@ -3,39 +3,46 @@ package scenes;
 import objects.Axis;
 import objects.IntersectableObject;
 import objects.IntersectableObjectDrawableOptions;
+import objects.entitiesObjects.BrickDrawableOptions;
+import objects.entitiesObjects.OBJLoader;
+import objects.entitiesObjects.StripedDrawableOptions;
 import objects.geometricsObjects.Cube;
 import objects.geometricsObjects.Plan;
+import objects.geometricsObjects.Triangle;
 import rendering.Ray;
 import utils.MyColor;
 import utils.MyVec3;
+
+import java.util.List;
 
 public class WhatACubeScene extends Scene{
 
     @Override
     protected void initSceneParameters() {
-        // we don't need to override this method
+        allowReflections = true;
     }
 
     @Override
     protected void initSceneObjects() {
-        IntersectableObjectDrawableOptions optionsPlan4 = new IntersectableObjectDrawableOptions(MyColor.teal, MyColor.lightGray, 10.0D);
+        StripedDrawableOptions optionsPlan4 = new StripedDrawableOptions(MyColor.teal, MyColor.lightGray, 10.0D, MyColor.lightCyan ,0.7D);
         IntersectableObject plan = new Plan(optionsPlan4, new MyVec3(0.0D, 1.0D, 0.0D), 1.5D);
 
         // Creation of the cubes
         IntersectableObjectDrawableOptions optionsCube = new IntersectableObjectDrawableOptions(MyColor.brightPink, MyColor.white, 10.0D);
-        MyVec3 center = new MyVec3(1D, -0.8D, -4.5D);
+        MyVec3 center = new MyVec3(1D, -0.83D, -4.5D);
         Cube cube = new Cube(optionsCube, center, 1D);
-        cube.rotateCube(-85, Axis.Y, new MyVec3(1D, -0.8D, -3.5D));
+        cube.rotateCube(-85, Axis.Y, new MyVec3(1D, -0.83D, -3.5D));
 
         IntersectableObjectDrawableOptions optionsCube2 = new IntersectableObjectDrawableOptions(MyColor.brightOlive, MyColor.white, 10.0D);
         // Creation of the cube
-        MyVec3 center2 = new MyVec3(-1D, -0.8D, -4.5D);
+        MyVec3 center2 = new MyVec3(-1D, -0.83D, -4.5D);
         Cube cube2 = new Cube(optionsCube2, center2, 1D);
-        cube2.rotateCube(85, Axis.Y, new MyVec3(-1D, -0.8D, -3.5D));
+        cube2.rotateCube(85, Axis.Y, new MyVec3(-1D, -0.83D, -3.5D));
 
-        IntersectableObjectDrawableOptions optionsCube3 = new IntersectableObjectDrawableOptions(MyColor.lightSkyBlue, MyColor.white, 10.0D);
-        MyVec3 center3 = new MyVec3(0D, -0.8D, -5.5D);
-        Cube cube3 = new Cube(optionsCube3, center3, 1D);
+
+        BrickDrawableOptions optionsBrick = new BrickDrawableOptions(MyColor.cyan, MyColor.white, 10.0D, MyColor.lightSkyBlue, 0.1D, 0.1D);
+        MyVec3 center3 = new MyVec3(0D, -0.83D, -5.5D);
+        Cube cube3 = new Cube(optionsBrick, center3, 1.2D);
 
         objects.add(plan);
         objects.add(cube);
