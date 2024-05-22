51f14d59464be721f13ac0b12d860d0c80cf701d
Al Natour Mazen <115185120+Al-Natour-Mazen@users.noreply.github.com>
2024-05-22 11:36:55 +0200

    Create Cube.java
    add new object cube

---
+++ src/objects/geometricsObjects/Cube.java
@@ -0,0 +1,142 @@
+package objects.geometricsObjects;
+
+import objects.Axis;
+import objects.IntersectableObject;
+import objects.IntersectableObjectDrawableOptions;
+import utils.MyVec3;
+
+public class Cube extends IntersectableObject {
+
+    private MyVec3 min; // Minimum point of the cube
+    private MyVec3 max; // Maximum point of the cube
+    private final MyVec3[] vertices; // Vertices of the cube
+
+    // Constructor to create a cube with specified options, center, and side length
+    public Cube(IntersectableObjectDrawableOptions options, MyVec3 center, double side) {
+        super(options);
+        double halfSide = side / 2;
+        vertices = new MyVec3[8];
+        vertices[0] = new MyVec3(center.getX() - halfSide, center.getY() - halfSide, center.getZ() - halfSide);
+        vertices[1] = new MyVec3(center.getX() + halfSide, center.getY() - halfSide, center.getZ() - halfSide);
+        vertices[2] = new MyVec3(center.getX() + halfSide, center.getY() + halfSide, center.getZ() - halfSide);
+        vertices[3] = new MyVec3(center.getX() - halfSide, center.getY() + halfSide, center.getZ() - halfSide);
+        vertices[4] = new MyVec3(center.getX() - halfSide, center.getY() - halfSide, center.getZ() + halfSide);
+        vertices[5] = new MyVec3(center.getX() + halfSide, center.getY() - halfSide, center.getZ() + halfSide);
+        vertices[6] = new MyVec3(center.getX() + halfSide, center.getY() + halfSide, center.getZ() + halfSide);
+        vertices[7] = new MyVec3(center.getX() - halfSide, center.getY() + halfSide, center.getZ() + halfSide);
+
+        updateMinMax();
+    }
+
+    private void updateMinMax() {
+        min = new MyVec3(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+        max = new MyVec3(Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY, Double.NEGATIVE_INFINITY);
+
+        for (MyVec3 vertex : vertices) {
+            min = new MyVec3(Math.min(min.getX(), vertex.getX()), Math.min(min.getY(), vertex.getY()), Math.min(min.getZ(), vertex.getZ()));
+            max = new MyVec3(Math.max(max.getX(), vertex.getX()), Math.max(max.getY(), vertex.getY()), Math.max(max.getZ(), vertex.getZ()));
+        }
+    }
+
+    public void rotateCube(double angle, Axis axis, MyVec3 center) {
+        double rad = Math.toRadians(angle);
+
+        for (int i = 0; i < vertices.length; i++) {
+            vertices[i] = vertices[i].subtract(center); // Translate to origin
+
+            double x = vertices[i].getX();
+            double y = vertices[i].getY();
+            double z = vertices[i].getZ();
+
+            switch (axis) {
+                case X:
+                    vertices[i] = new MyVec3(x,
+                            y * Math.cos(rad) - z * Math.sin(rad),
+                            y * Math.sin(rad) + z * Math.cos(rad));
+                    break;
+                case Y:
+                    vertices[i] = new MyVec3(z * Math.sin(rad) + x * Math.cos(rad),
+                            y,
+                            z * Math.cos(rad) - x * Math.sin(rad));
+                    break;
+                case Z:
+                    vertices[i] = new MyVec3(x * Math.cos(rad) - y * Math.sin(rad),
+                            x * Math.sin(rad) + y * Math.cos(rad),
+                            z);
+                    break;
+            }
+
+            vertices[i] = vertices[i].add(center); // Translate back to original position
+        }
+
+        updateMinMax();
+    }
+
+    @Override
+    public double getIntersection(MyVec3 origin, MyVec3 direction) {
+        // Implement the intersection logic (unchanged)
+        double tMin = (min.getX() - origin.getX()) / direction.getX();
+        double tMax = (max.getX() - origin.getX()) / direction.getX();
+
+        if (tMin > tMax) {
+            double temp = tMin;
+            tMin = tMax;
+            tMax = temp;
+        }
+
+        double tyMin = (min.getY() - origin.getY()) / direction.getY();
+        double tyMax = (max.getY() - origin.getY()) / direction.getY();
+
+        if (tyMin > tyMax) {
+            double temp = tyMin;
+            tyMin = tyMax;
+            tyMax = temp;
+        }
+
+        if ((tMin > tyMax) || (tyMin > tMax))
+            return -1;
+
+        if (tyMin > tMin)
+            tMin = tyMin;
+
+        if (tyMax < tMax)
+            tMax = tyMax;
+
+        double tzMin = (min.getZ() - origin.getZ()) / direction.getZ();
+        double tzMax = (max.getZ() - origin.getZ()) / direction.getZ();
+
+        if (tzMin > tzMax) {
+            double temp = tzMin;
+            tzMin = tzMax;
+            tzMax = temp;
+        }
+
+        if ((tMin > tzMax) || (tzMin > tMax))
+            return -1;
+
+        if (tzMin > tMin)
+            tMin = tzMin;
+
+        if (tzMax < tMax)
+            tMax = tzMax;
+
+        return tMin;
+    }
+
+    @Override
+    public MyVec3 getNormal(MyVec3 I) {
+        double epsilon = 0.0001;
+        if (Math.abs(I.getX() - min.getX()) < epsilon)
+            return new MyVec3(-1, 0, 0);
+        else if (Math.abs(I.getX() - max.getX()) < epsilon)
+            return new MyVec3(1, 0, 0);
+        else if (Math.abs(I.getY() - min.getY()) < epsilon)
+            return new MyVec3(0, -1, 0);
+        else if (Math.abs(I.getY() - max.getY()) < epsilon)
+            return new MyVec3(0, 1, 0);
+        else if (Math.abs(I.getZ() - min.getZ()) < epsilon)
+            return new MyVec3(0, 0, -1);
+        else
+            return new MyVec3(0, 0, 1);
+    }
+}
